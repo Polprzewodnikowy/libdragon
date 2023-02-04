1e039a0e840ae362cfac92c62a2501399ce8321e
Polprzewodnikowy <Polprzewodnikowy@users.noreply.github.com>
2023-02-04 00:11:40 +0000

    deploy: e686c608ca239f02e793ee2e0cc9f26dcfe7fdf0

---
+++ ref/search/defines_6.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['num_5fbuffers_0',['NUM_BUFFERS',['../display_8c.html#aac028f4c15ac7f78a44be5db76cfbec7',1,'display.c']]]
+];
---
+++ ref/search/enums_7.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['rtc_5ftype_5ft_0',['rtc_type_t',['../group__rtc.html#gaf4e6e809a36c78b63cd09260d3a5f561',1,'rtc.c']]]
+];
